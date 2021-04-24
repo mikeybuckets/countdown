23c49848ddaac5f9d8d99f138597dbae7822fc09
xeptao <xeptaowastaken@gmail.com>
2021-04-24 20:09:24 +0300

    Add: JSX Markup.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "./../node_modules/inter-ui/inter.css";
+import "./main.css";
+import "./../node_modules/xeptaoreset.css/dist/xeptaoreset.min.css";
 
 const App = () => {
 	return (
 		<>
-			<h1>Let's Goo!</h1>
+			<h1>it's getting closer and closer.</h1>
+			<div className="countdown-wrapper">
+				<div className="months-wrapper">
+					<div className="months">00</div>
+					<span className="label">months</span>
+				</div>
+				<div className="days-wrapper">
+					<div className="days">00</div>
+					<span className="label">days</span>
+				</div>
+				<div className="hours-wrapper">
+					<div className="hours">00</div>
+					<span className="label">hours</span>
+				</div>
+				<div className="minutes-wrapper">
+					<div className="minutes">00</div>
+					<span className="label">minutes</span>
+				</div>
+				<div className="seconds-wrapper">
+					<div className="seconds">00</div>
+					<span className="label">seconds</span>
+				</div>
+			</div>
 		</>
 	);
 };
